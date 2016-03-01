5f550d8bbae9069fd66a4c2fd3f259167e990077
Surrui <Surrui@HAL9000>
2016-03-01 22:06:13 +0100

    Commit: No lee archivos xml

--- Confusion/src/main/java/net/jimenez/Confusion/MaestroController.java
+++ Confusion/src/main/java/net/jimenez/Confusion/MaestroController.java
@@ -2,13 +2,17 @@ package net.jimenez.Confusion;
 
 import javafx.fxml.FXML;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import javafx.event.ActionEvent;
-
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -25,52 +29,45 @@ public class MaestroController {
 
 	// Event Listener on Button.onAction
 	@FXML
-	public void escollirFitxer(ActionEvent event) {
-		
+	public void escollirFitxer(ActionEvent event) throws ParserConfigurationException, SAXException, IOException {
+
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.getExtensionFilters().addAll(
-				new ExtensionFilter("TXT Files", "*.txt"));
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("XML Files", "*.xml"));
 
 		File selectedFile = fileChooser.showOpenDialog(null);
 
-		BufferedReader u = null;
-		
-		String r = null;
-		
-		try {
-		
-		u = new BufferedReader(new FileReader(selectedFile));
-		
-		while ((r = u.readLine()) != null) {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(selectedFile);
+		doc.getDocumentElement().normalize();
+
+		NodeList nList = doc.getElementsByTagName("profesor");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			System.out.println("Inicio");
 			
-			String [] dades = r.split(", ");
-			
-			System.out.println(dades[0]);
-			
-		}
-		
-		} catch (IOException e) {
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-			e.printStackTrace();
+				Element eElement = (Element) nNode;
 
-		} finally {
-			
-			try {
-
-				if (u != null) u.close();
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
+				System.out
+						.println("Nom: " + getTagValue("nom", eElement) + " - Sexe: " + getTagValue("sexe", eElement));
 
 			}
-			
 		}
 		
-		/*if (selectedFile != null) {
-			actionStatus.setText("File selected: " + selectedFile.getName());
-		} else {
-			actionStatus.setText("File selection cancelled.");
-		}*/
+		System.out.println("Fin");	
 	}
+
+	private String getTagValue(String sTag, Element eElement) {
+		NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+
+		Node nValue = (Node) nlList.item(0);
+
+		return nValue.getNodeValue();
+	}
+
 }
